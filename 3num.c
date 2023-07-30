05cbde1091c4d1fd06fb15faac7f148542a19b74
Oswaldo Jesús Marín Pagés <83247451+ozzies-code@users.noreply.github.com>
2023-07-30 13:09:21 -0400

    Project files uploaded for upload
    Program files have been uploaded. The program performs the comparison of 3 numbers to determine which of them is the greater number.

---
+++ 3num.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{   
+    int a, b, c, mayor;     
+    printf(" Introduzca tres numeros enteros:");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b)
+    if (a > c) mayor = a;
+    else mayor = c;
+    else
+    if (a > c) mayor = a;
+    {
+       printf("%.2f %s", numero, "es mayor que cero\n");
+       puts("Pruebe de nuevo introducioendo un numero negativo"); 
+    }
+    else if (numero < 0)
+     {
+       printf("%.2f %s", numero, "es menor que cero\n");
+       puts("Pruebe de nuevo introducioendo un numero positivo"); 
+    }
+    else
+     {
+       printf("%.2f %s", numero, "es igual a cero\n");
+       puts("porque no introduce otro numero?"); 
+    }
+    
+}
